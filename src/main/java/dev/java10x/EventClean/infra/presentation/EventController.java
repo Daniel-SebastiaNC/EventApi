57a42b8233f4b007a6e214f624b253e199ad9400
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-03-15 19:13:31 -0300

    feat: adiciona ResponseEntity para requisições da api

--- src/main/java/dev/java10x/EventClean/infra/presentation/EventController.java
+++ src/main/java/dev/java10x/EventClean/infra/presentation/EventController.java
@@ -6,9 +6,12 @@ import dev.java10x.EventClean.core.usecases.FindEventUsecase;
 import dev.java10x.EventClean.infra.dtos.EventDto;
 import dev.java10x.EventClean.infra.mapper.EventDtoMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,13 +23,21 @@ public class EventController {
     private final EventDtoMapper eventDtoMapper;
 
     @PostMapping("/create")
-    public EventDto createEvent(@RequestBody EventDto eventDto){
+    public ResponseEntity<Map<String, Object>> createEvent(@RequestBody EventDto eventDto){
         Event newEvent = createEventUsecase.execute(eventDtoMapper.toDomain(eventDto));
-        return eventDtoMapper.toEventDto(newEvent);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("Message: ", "Event Created");
+        response.put("Data: ", eventDtoMapper.toEventDto(newEvent));
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping("/list")
-    public List<EventDto> findEvent(){
-        return findEventUsecase.execute().stream().map(eventDtoMapper::toEventDto).toList();
+    public ResponseEntity<Map<String, Object>> findEvent(){
+        Map<String, Object> response = new HashMap<>();
+        response.put("Message: ", "Events Found");
+        response.put("Data: ", findEventUsecase.execute().stream().map(eventDtoMapper::toEventDto).toList());
+
+        return ResponseEntity.ok(response);
     }
 }
