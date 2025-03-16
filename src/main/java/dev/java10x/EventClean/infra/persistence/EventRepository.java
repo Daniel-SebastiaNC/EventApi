56d42a83ef18bebe8ab4aa8d00626f305aaf3ddc
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-03-16 16:43:16 -0300

    feat: adiciona buscar persolalizado pelo atributo identifier de Event

--- src/main/java/dev/java10x/EventClean/infra/persistence/EventRepository.java
+++ src/main/java/dev/java10x/EventClean/infra/persistence/EventRepository.java
@@ -3,6 +3,9 @@ package dev.java10x.EventClean.infra.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
+    Optional<EventEntity> findEventByIdentifier(String identifier);
 }
