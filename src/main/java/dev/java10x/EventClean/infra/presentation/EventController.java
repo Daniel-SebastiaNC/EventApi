094fdeca351c9a435cd5241e7c1e538133dc5aa3
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-03-17 11:08:02 -0300

    feat: adicona o Usecase RandomIdentifierUsecase para gerar o identifier no endpoint de createEvent (POST)

--- src/main/java/dev/java10x/EventClean/infra/presentation/EventController.java
+++ src/main/java/dev/java10x/EventClean/infra/presentation/EventController.java
@@ -4,6 +4,7 @@ import dev.java10x.EventClean.core.domains.Event;
 import dev.java10x.EventClean.core.usecases.CreateEventUsecase;
 import dev.java10x.EventClean.core.usecases.FindEventByIdentifierUsecase;
 import dev.java10x.EventClean.core.usecases.FindEventUsecase;
+import dev.java10x.EventClean.core.usecases.RandomIdentifierUsecase;
 import dev.java10x.EventClean.infra.dtos.EventDto;
 import dev.java10x.EventClean.infra.mapper.EventDtoMapper;
 import lombok.RequiredArgsConstructor;
@@ -22,12 +23,15 @@ public class EventController {
     private final CreateEventUsecase createEventUsecase;
     private final FindEventUsecase findEventUsecase;
     private final FindEventByIdentifierUsecase findEventByIdentifierUsecase;
+    private final RandomIdentifierUsecase randomIdentifierUsecase;
     private final EventDtoMapper eventDtoMapper;
 
     @PostMapping("/create")
     public ResponseEntity<Map<String, Object>> createEvent(@RequestBody EventDto eventDto){
 
-        Event newEvent = createEventUsecase.execute(eventDtoMapper.toDomain(eventDto));
+        String identifier = randomIdentifierUsecase.execute();
+
+        Event newEvent = createEventUsecase.execute(eventDtoMapper.toDomain(eventDto, identifier));
 
         Map<String, Object> response = new HashMap<>();
         response.put("Message: ", "Event Created");
